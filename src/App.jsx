c2396864d0fa92ad6b17f5ec9559203d90ad6235
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-08 14:56:53 -0500

    Create main components

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Service from './components/service/Service';
+import Portfolio from '//portfolio/Portfolio';
+import Testimonial from './components/testimonial/Testimonial';
+import Contact from '//contact/Contact';
+import Footer from '//footer/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Service />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
